767b5f6aaa48e2a499b270f749457556fbbeda9e
cerveau08 <malickcoly342@gmail.com>
2020-09-14 21:32:21 +0000

    init and text and image send ok

---
+++ lib/const.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final themeColor = Colors.green[50];
+final primaryColor = Colors.black;
+final greyColor2 = Colors.green[100];
+final greyColor = Colors.yellow;
